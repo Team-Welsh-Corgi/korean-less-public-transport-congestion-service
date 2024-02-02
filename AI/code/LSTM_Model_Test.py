63149b3ac1a57b4d9423dd62b1ec316029ca67e2
KN-20 <2020112323@student.kyungnam.ac.kr>
2024-02-02 17:33:31 +0900

    쉽게 사용하는 ai 제작중 + test data
    초보도 쉽게 사용할 수 있는 ai툴 제작중
    LSTM 한정
    + test data 추가 저장

--- AI/code/LSTM_Model_Test.py
+++ AI/code/LSTM_Model_Test.py
@@ -2,6 +2,8 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import os
+import time
+
 from sklearn.model_selection import train_test_split
 from keras.models import Model
 from keras.layers import Input, Dense, LSTM, Bidirectional
@@ -10,22 +12,22 @@ from keras import backend as K
 
 class Bus:
     def __init__(self):
-        pass
+        self.X_train = None
+        self.y_train = None
+        self.X_test = None
+        self.y_test = None
+        self.model = None
+        self.result = None
 
     # 버스 혼잡도 csv파일 전처리 과정
-    def normal_data_preprocessing(self, path, prepro_check):
+    def data_preprocessing(self, path, prepro_check):
         main_data = pd.read_csv('../data/' + os.listdir(path)[0], names=['계절', '시간', '버스', '정류장', '혼잡도',
                                                                          '평휴일', '이전휴일수', '이후휴일수'])
         for i in os.listdir(path)[1:]:
-            # 경기도 버스 csv 파일을 거르기 위한 if문
-            if 'G' not in i:
-                # 종종 csv 파일에 공백이 있는경우가 있는데 그러면 오류 나와서 이를 해결하기위해 만듦
-                try:
-                    temp_data = pd.read_csv(path + i, names=['계절', '시간', '버스', '정류장', '혼잡도',
-                                                             '평휴일', '이전휴일수', '이후휴일수'])
-                    main_data = pd.concat([main_data, temp_data], axis=0, ignore_index=True)
-                except:
-                    continue
+            temp_data = pd.read_csv(path + i, names=['계절', '시간', '버스', '정류장', '혼잡도',
+                                                     '평휴일', '이전휴일수', '이후휴일수'])
+            main_data = pd.concat([main_data, temp_data], axis=0, ignore_index=True)
+            print(i + '파일을 읽는 중 입니다.')
 
         main_data = main_data.astype('int32')
 
@@ -42,16 +44,59 @@ class Bus:
         X = X.reshape(len(X), len(X[0]), 1)
 
         self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(X, y, random_state=777)
+        if self.data_check():
+            print('전처리 완료')
+            self.data_result()
+        else:
+            print('전처리 과정에서 문제가 발생했습니다.')
+
+    def data_result(self):
+        check_label = [0 for i in range(100)]
+        for i in self.y_train:
+            check_label[i] += 1
+        for i in self.y_test:
+            check_label[i] += 1
+
+        print('총 데이터의 양: ' + str(len(self.X_train) + len(self.X_test)))
+        print('학습용 데이터의 양: ' + str(len(self.X_train)))
+        print('테스트용 데이터의 양' + str(len(self.X_test)))
+        print('\n라벨링 개수')
+        for i in range(len(check_label)):
+            if check_label[i] == 0:
+                continue
+            print(str(i) + '번: ' + str(check_label[i]))
+
+    def data_save(self):
+        if self.data_check():
+            print('데이터가 잘못 처리되었습니다.')
+        else:
+            time_now = time.strftime('%y%m%d_%h%M%S', time.localtime(time.time()))
+            np.save('../data/preprocessing/X_train' + time_now, self.X_train)
+            print('파일이 저장되었습니다.\n저장 위치: data/preprocessing/X_train' + time_now)
+            np.save('../data/preprocessing/y_train' + time_now, self.y_train)
+            print('파일이 저장되었습니다.\n저장 위치: data/preprocessing/y_train' + time_now)
+            np.save('../data/preprocessing/X_test' + time_now, self.X_test)
+            print('파일이 저장되었습니다.\n저장 위치: data/preprocessing/X_test' + time_now)
+            np.save('../data/preprocessing/y_test' + time_now, self.y_test)
+            print('파일이 저장되었습니다.\n저장 위치: data/preprocessing/y_test' + time_now)
+
+    def data_check(self):
+        if (len(self.X_train) == len(self.y_train)) | (len(self.X_test) == len(self.y_test)) | \
+                (len(self.X_train) == 0) | (len(self.y_train) == 0) | \
+                (len(self.X_test) == 0) | (len(self.y_test) == 0):
+            return False
+        return True
 
     # 모델 설계 과정
     def model_design(self):
         K.clear_session()
-        xInput = Input(batch_shape=(None, self.X_train.shape[1], self.X_train.shape[2]))
-        xLstm_1 = LSTM(10, return_sequences=True)(xInput)
-        xLstm_2 = Bidirectional(LSTM(10))(xLstm_1)
-        xOutput = Dense(1)(xLstm_2)
-        self.model = Model(xInput, xOutput)
+        xinput = Input(batch_shape=(None, self.X_train.shape[1], self.X_train.shape[2]))
+        xlstm_1 = LSTM(len(self.X_train), return_sequences=True)(xinput)
+        xlstm_2 = Bidirectional(LSTM(len(self.X_train)))(xlstm_1)
+        xoutput = Dense(1)(xlstm_2)
+        self.model = Model(xinput, xoutput)
         self.model.compile(loss='mse', optimizer='adam')
+        self.model.summary()
 
     # 모델 학습
     def model_training(self):
@@ -62,6 +107,10 @@ class Bus:
         self.result = self.model.predict(self.X_test, batch_size=1)
         self.show_result()
 
+    def model_save(self):
+        time_now = time.strftime('%y%m%d_%h%M%S', time.localtime(time.time()))
+        self.model.save('../model' + time_now + '.h5')
+
     # 결과를 그래프로 변환
     def show_result(self):
         b_axis = np.arange(0, len(self.result[0:200]))
@@ -71,7 +120,7 @@ class Bus:
         plt.legend()
         plt.show()
 
-    # class
+    # 한번에 다실행하는 클래스 사용하진 않지만 테스트용으로 남겨놨음
     def bus_main(self, path, prepro_check):
         self.data_preprocessing(path, prepro_check)
         self.model_design()
--- AI/code/main.py
+++ AI/code/main.py
@@ -1,19 +1,51 @@
 import LSTM_Model_Test
-import os
-if __name__ == '__ main__':
-    while True:
-        check = None
-        prepro_check = None
-        print('원하는 설계를 입력하시오.')
-        print('1. 전처리')
-        print('2. 전처리 데이터 저장')
-        print('3. 모델 설계 및 테스트')
-        print('4. 모델 저장')
 
-        check = int(input('>>'))
 
-        if check == 1:
-            print('전처리 과정을 선택하시오.')
-            print('1. 출퇴근 시간을 포함한 전처리 데이터 만들기')
-            print('2. 출퇴근 시간을 포함하지 않은 전처리 데이터 만들기')
-            prepro_check = int(input('>>'))
+LMT = LSTM_Model_Test.Bus()
+while True:
+    prepro_check = None
+    print('원하는 설계를 입력하시오.')
+    print('1. 전처리')
+    print('2. 전처리 데이터 저장')
+    print('3. 모델 설계 및 테스트')
+    print('4. 모델 저장')
+    print('5. 종료')
+
+    check = int(input('>> '))
+
+    if check == 1:
+        print('1. 서울버스')
+        print('2. 경기버스')
+        path_check = int(input('>> '))
+        if path_check == 1:
+            path = '../data/seoul/'
+        elif path_check == 2:
+            path = '../data/gyeonggi/'
+        else:
+            print('잘못 입력했습니다.')
+            continue
+
+        print('전처리 과정을 선택하시오.(data 폴더안에 서울과 경기도 위치에 따라 csv 파일을 넣어주세요.)')
+        print('1. 출퇴근 시간을 포함하지 않은 전처리 데이터 만들기')
+        print('2. 출퇴근 시간을 포함한 전처리 데이터 만들기')
+        prepro_check = int(input('>> '))
+
+        if prepro_check != 1 | prepro_check != 2:
+            print('잘못 입력했습니다.')
+            continue
+
+        print('전처리를 시작합니다.')
+        LMT.data_preprocessing(path=path, prepro_check=prepro_check)
+    elif check == 2:
+        LMT.data_save()
+    elif check == 3:
+        print('모델 설계를 시작합니다.')
+        LMT.model_design()
+        LMT.model_training()
+        LMT.model_result()
+    elif check == 4:
+        LMT.model_save()
+    elif check == 5:
+        break
+    else:
+        print('잘못 입력했습니다.')
